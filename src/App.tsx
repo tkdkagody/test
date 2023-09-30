91874761628de2f0a076ab331f3eb7296711f900
tkdkagody <80307779+tkdkagody@users.noreply.github.com>
2023-09-30 15:33:14 +0900

    add: Add env

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const token = import.meta.env.VITE_USER_TOKEN;
+  console.log(token);
   const [count, setCount] = useState(0);
 
   return <> github action + firebase ci/cd 성공했으면 좋겠다, </>;
